6fb12e7495e10199404922b01de3a36a6b7e2b2f
Chris Green <chrisgreen@chriss-air.home>
2024-03-09 22:00:18 -0500

    Added the title overlaying the map inside a pill shape container

--- lib/Screens/HomeScreen.dart
+++ lib/Screens/HomeScreen.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/material.dart';
 import 'package:artswindsoressex/constants.dart';
+import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -7,7 +7,7 @@ class HomeScreen extends StatefulWidget {
 
   static const _initialCameraPosition = CameraPosition(
     target: LatLng(42.24656086052182, -83.01904262309124),
-    zoom: 18.0,
+    zoom: 17.0,
   );
 
   const HomeScreen({super.key});
@@ -17,13 +17,41 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  String locationName = "St. Clair College";
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: GoogleMap(
-        myLocationButtonEnabled: false,
-        zoomControlsEnabled: true,
-        initialCameraPosition: HomeScreen._initialCameraPosition,
+    return Scaffold(
+      body: Stack(
+        children: [
+          const GoogleMap(
+            myLocationButtonEnabled: false,
+            zoomControlsEnabled: true,
+            initialCameraPosition: HomeScreen._initialCameraPosition,
+          ),
+          Positioned(
+            top: 50.0,
+            left: 50.0,
+            right: 50.0,
+            child: Container(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+              decoration: BoxDecoration(
+                color: Colors.white.withOpacity(0.75),
+                borderRadius: BorderRadius.circular(20.0),
+              ),
+              child: Text(
+                locationName,
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  fontSize: size16,
+                  fontWeight: FontWeight.bold,
+                  color: textColor,
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
