170202818eb7c016aa0fbb988155bacfad1830a2
Chris Green <chrisgreen@chriss-air.home>
2024-03-11 00:36:27 -0400

    Added floating action button * The floating action button will navigate the user back to the starting location of the map * Added the Information icon to the top right of the screen that navigates the user to the AboutApp Screen

--- lib/Screens/HomeScreen.dart
+++ lib/Screens/HomeScreen.dart
@@ -1,6 +1,8 @@
-import 'package:artswindsoressex/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:artswindsoressex/constants.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+import 'AboutApp.dart';
 
 class HomeScreen extends StatefulWidget {
   static const id = "HomeScreen";
@@ -19,39 +21,73 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   String locationName = "St. Clair College";
 
+  late GoogleMapController _googleMapController;
+
+  @override
+  void dispose() {
+    _googleMapController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: [
-          const GoogleMap(
-            myLocationButtonEnabled: false,
-            zoomControlsEnabled: false,
-            initialCameraPosition: HomeScreen._initialCameraPosition,
-          ),
-          Positioned(
-            top: 50.0,
-            left: 50.0,
-            right: 50.0,
-            child: Container(
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-              decoration: BoxDecoration(
-                color: Colors.white.withOpacity(0.75),
-                borderRadius: BorderRadius.circular(20.0),
-              ),
-              child: Text(
-                locationName,
-                textAlign: TextAlign.center,
-                style: const TextStyle(
-                  fontSize: size16,
-                  fontWeight: FontWeight.bold,
-                  color: textColor,
+    return MaterialApp(
+      debugShowCheckedModeBanner: false, // Hide debug banner
+      home: Scaffold(
+        body: Stack(
+          children: [
+            GoogleMap(
+              myLocationButtonEnabled: false,
+              zoomControlsEnabled: false,
+              initialCameraPosition: HomeScreen._initialCameraPosition,
+              onMapCreated: (controller) => _googleMapController = controller,
+            ),
+            Positioned(
+              top: 50.0,
+              left: 50.0,
+              right: 50.0,
+              child: Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                decoration: BoxDecoration(
+                  color: Colors.white.withOpacity(0.75),
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+                child: Text(
+                  locationName,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    fontSize: size16,
+                    fontWeight: FontWeight.bold,
+                    color: textColor,
+                  ),
                 ),
               ),
             ),
-          ),
-        ],
+            Row(
+              children: [
+                const Spacer(),
+                IconButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, AboutApp.id);
+                  },
+                  icon: const Icon(Icons.info_outline_rounded),
+                ),
+              ],
+            ),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            // Animate the map to the initial camera position
+            _googleMapController.animateCamera(
+              CameraUpdate.newCameraPosition(HomeScreen._initialCameraPosition),
+            );
+          },
+          backgroundColor: Colors.white, // Change the background color
+          child: const Icon(Icons.my_location_rounded), // Change the icon
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
       ),
     );
   }
