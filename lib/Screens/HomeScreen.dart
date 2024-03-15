601f0882c58d035593ce9e353b583bc648017d39
Chris Green <chrisgreen@chriss-air.home>
2024-03-14 22:20:58 -0400

    Disabled the googleToolBar * When a pin is clicked, the google maps tool bar will not appear

--- lib/Screens/HomeScreen.dart
+++ lib/Screens/HomeScreen.dart
@@ -31,72 +31,74 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Stack(
-          children: [
-            GoogleMap(
-              myLocationButtonEnabled: false,
-              zoomControlsEnabled: false,
-              initialCameraPosition: HomeScreen._initialCameraPosition,
-              onMapCreated: (controller) => _googleMapController = controller,
-              markers: Set<Marker>.from(locations.map((location) => Marker(
-                    markerId: MarkerId(location.title),
-                    position: LatLng(double.parse(location.latitude),
-                        double.parse(location.longitude)),
-                    infoWindow: InfoWindow(title: location.title),
-                  ))),
-            ),
-            Positioned(
-              top: 70.0,
-              left: 50.0,
-              right: 50.0,
-              child: Container(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-                decoration: BoxDecoration(
-                  color: Colors.white.withOpacity(0.75),
-                  borderRadius: BorderRadius.circular(20.0),
-                ),
-                child: Text(
-                  locationName,
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(
-                    fontSize: size16,
-                    fontWeight: FontWeight.bold,
-                    color: textColor,
-                  ),
+      body: Stack(
+        children: [
+          GoogleMap(
+            mapToolbarEnabled: false,
+            myLocationButtonEnabled: false,
+            zoomControlsEnabled: false,
+            initialCameraPosition: HomeScreen._initialCameraPosition,
+            onMapCreated: (controller) => _googleMapController = controller,
+            markers: Set<Marker>.from(locations.map((location) => Marker(
+                  markerId: MarkerId(location.title),
+                  position: LatLng(double.parse(location.latitude),
+                      double.parse(location.longitude)),
+                  infoWindow: InfoWindow(title: location.title),
+                ))),
+          ),
+          Positioned(
+            top: 70.0,
+            left: 50.0,
+            right: 50.0,
+            child: Container(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+              decoration: BoxDecoration(
+                color: Colors.white.withOpacity(0.75),
+                borderRadius: BorderRadius.circular(20.0),
+              ),
+              child: Text(
+                locationName,
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  fontSize: size16,
+                  fontWeight: FontWeight.bold,
+                  color: textColor,
                 ),
               ),
             ),
-            Container(
-              padding: EdgeInsets.all(25),
-              child: Row(
-                children: [
-                  const Spacer(),
-                  IconButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, AboutApp.id);
-                    },
-                    icon: const Icon(Icons.info_outline_rounded),
-                  ),
-                ],
-              ),
-            )
-          ],
-        ),
-        floatingActionButton: Container(
-            padding: EdgeInsets.only(bottom: 75,right: 10),
-            child: FloatingActionButton(
-              onPressed: () {
-                // Animate the map to the initial camera position
-                _googleMapController.animateCamera(
-                  CameraUpdate.newCameraPosition(HomeScreen._initialCameraPosition),
-                );
-              },
-              backgroundColor: orangeColor.withOpacity(0.90), // Change the background color
-              child: const Icon(Icons.my_location_rounded, color: Colors.white),
+          ),
+          Container(
+            padding: EdgeInsets.all(25),
+            child: Row(
+              children: [
+                const Spacer(),
+                IconButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, AboutApp.id);
+                  },
+                  icon: const Icon(Icons.info_outline_rounded),
+                ),
+              ],
             ),
+          )
+        ],
+      ),
+      floatingActionButton: Container(
+        padding: EdgeInsets.only(bottom: 75, right: 10),
+        child: FloatingActionButton(
+          onPressed: () {
+            // Animate the map to the initial camera position
+            _googleMapController.animateCamera(
+              CameraUpdate.newCameraPosition(HomeScreen._initialCameraPosition),
+            );
+          },
+          backgroundColor:
+              orangeColor.withOpacity(0.90), // Change the background color
+          child: const Icon(Icons.my_location_rounded, color: Colors.white),
         ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
-      );
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+    );
   }
 }
