250a20f5598b5ccc2e7a39bbe942bd698ee12efb
YashPindiwala <w0790912@myscc.ca>
2024-03-17 10:46:20 -0400

    added new endpoints for artwork

--- lib/API/Endpoints.dart
+++ lib/API/Endpoints.dart
@@ -1,6 +1,9 @@
 enum Endpoint {
   GET_EVENT_CURR,
   GET_EVENT_PAST,
+  GET_ARTWORK_DIGITAL,
+  GET_ARTWORK_NOT_DIGITAL,
+  GET_ARTWORK_ALL
 }
 
 String toString(Endpoint endpoint) {
@@ -9,6 +12,12 @@ String toString(Endpoint endpoint) {
       return "events?status=current";
     case Endpoint.GET_EVENT_PAST:
       return "events?status=past";
+    case Endpoint.GET_ARTWORK_ALL:
+      return "artworks?type=all";
+    case Endpoint.GET_ARTWORK_DIGITAL:
+      return "artworks?type=digital";
+    case Endpoint.GET_ARTWORK_DIGITAL:
+      return "artworks?type=notDigital";
     default:
       return "";
   }
